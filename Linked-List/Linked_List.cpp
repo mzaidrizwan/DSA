39972e1a0448f1b6d6da6eb8c430c7a324a6e33d
mzaidrizwan <z0887451@gmail.com>
2026-01-24 16:40:15 +0500

    Linked-list: successfuly created nodes and also Traversed them.

---
+++ Linked-List/Linked_List.cpp
@@ -0,0 +1,37 @@
+#include <cstddef>
+#include <iostream>
+using namespace std;
+
+struct Node {
+  int data;
+  Node *next;
+};
+
+void printList(Node *head) {
+  Node *currentNode = head;
+
+  while (currentNode != NULL) {
+    cout << currentNode->data<<" - ";
+    currentNode = currentNode->next;
+  }
+}
+
+int main() {
+
+  Node *head = new Node();
+  Node *second = new Node();
+  Node *third = new Node();
+
+  head->data = 10;
+  head->next = second;
+
+  second->data = 20;
+  second->next = third;
+
+  third->data = 30;
+  third->next = NULL;
+
+  printList(head);
+
+  return 0;
+}
