093498d7ae86b3d01bd96ead53c6ba611aa73c3e
mzaidrizwan <z0887451@gmail.com>
2026-02-07 13:21:17 +0500

    Now I can add on demand numbers of nodes,currently can only add nodes at the Beginning.

--- Linked-List/Linked_List.cpp
+++ Linked-List/Linked_List.cpp
@@ -2,36 +2,67 @@
 #include <iostream>
 using namespace std;
 
+// CREATING CUSTOM DATA TYPE
 struct Node {
   int data;
   Node *next;
 };
 
+// PRINT LINKEDLIST
 void printList(Node *head) {
   Node *currentNode = head;
 
   while (currentNode != NULL) {
-    cout << currentNode->data<<" - ";
+    cout << currentNode->data << " - ";
     currentNode = currentNode->next;
   }
 }
 
+// CUSTOM NODES CREATOR
+Node *createNode(int data) {
+  Node *node = new Node();
+  node->data = data;
+  node->next = nullptr;
+  return node;
+}
+
 int main() {
 
+  // DECLARE HEAD NODE
   Node *head = new Node();
-  Node *second = new Node();
-  Node *third = new Node();
 
-  head->data = 10;
-  head->next = second;
+  // INITIALIZE HEADH
+  head->data = NULL;
+  head->next = nullptr;
+  while (1) {
 
-  second->data = 20;
-  second->next = third;
+    cout << "add new data\n";
+    int userInputData;
+    cin >> userInputData;
 
-  third->data = 30;
-  third->next = NULL;
+    Node *newNode = createNode(userInputData);
+    if (head->data == NULL) {
+      head->data = newNode->data;
+      continue;
+    }
 
-  printList(head);
+    cout << "position of this new data?\n1. Beginning\n2. End\n3. I want to tell position";
+    int userInputPosition;
+    cin >> userInputPosition;
 
+    if (userInputPosition == 1) {
+      int temp_stored_data = newNode->data;
+      Node *temp_stored_next = newNode;
+
+      newNode->data = head->data;
+      newNode->next = head->next;
+
+      head->data = temp_stored_data;
+      head->next = temp_stored_next;
+    }
+
+    printList(head);
+    break;
+  }
   return 0;
 }
