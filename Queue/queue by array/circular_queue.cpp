881324c1ca2e82f5611a4c00a1c08baed7bcb5cc
mzaidrizwan <z0887451@gmail.com>
2026-03-01 16:10:12 +0500

    available manipulations: 1. Engueue 2. dequeue 3. Print Queue 4. Check Either the Queue is Empty or Not

--- Queue/queue by array/circular_queue.cpp
+++ Queue/queue by array/circular_queue.cpp
@@ -3,33 +3,33 @@
 using namespace std;
 
 int queue[3];
-int rear = -1, front = -1,numberOfElement=0;
+int rear = -1, front = -1, numberOfElement = 0;
 int length = sizeof(queue) / sizeof(int);
 
 void enqueue(int data) {
 
-  if (length!=numberOfElement) {
+  if (length != numberOfElement) {
 
-      if (rear != -1) {
-        rear = (rear + 1) % length;
+    if (rear != -1) {
+      rear = (rear + 1) % length;
       queue[rear] = data;
     } else {
       queue[++rear] = data;
       front = rear;
     }
-++numberOfElement;
+    ++numberOfElement;
 
   } else
     cout << " overflow ";
 }
 
-void dequeue(){
-  if (front!=-1) {
-    queue[front]=-1;
-    front=(front+1)%length;
+void dequeue() {
+  if (front != -1) {
+    queue[front] = -1;
+    front = (front + 1) % length;
     --numberOfElement;
-  }else {
-  cout << " underflow ";
+  } else {
+    cout << " underflow ";
   }
 }
 
@@ -37,8 +37,8 @@ int main() {
 
   while (1) {
 
-    cout << "\n1. Engueue\n2. dequeue\n3. Print Queue\n4. Return the Front"
-            "element.\n5. Check Either the Queue is Empty or Not.\n";
+    cout << "\n1. Engueue\n2. dequeue\n3. Print Queue\n4. Check Either the "
+            "Queue is Empty or Not.\n";
     int userInp;
     cin >> userInp;
 
@@ -49,15 +49,18 @@ int main() {
       cin >> userInpData;
 
       enqueue(userInpData);
-    } else if (userInp == 2) {
-dequeue();
-       } else if (userInp == 3) {
+    } else if (userInp == 2)dequeue();
+    else if (userInp == 3) {
 
-        cout<<"\n front "<<front<< " | "<<" rear "<<rear<<endl;
+      cout << "\n front " << front << " | " << " rear " << rear << endl;
 
-      for (int i = front,j=numberOfElement; (i <= rear || i >= front)&&j; i = (i + 1) % length) {
-        cout << queue[i] << "~";--j;
+      for (int i = front, j = numberOfElement; (i <= rear || i >= front) && j;
+           i = (i + 1) % length) {
+        cout << queue[i] << "~";
+        --j;
       }
+    }else if (userInp==4) {
+    cout << numberOfElement << "/"<< length<<" is used.";
     }
   }
 }
